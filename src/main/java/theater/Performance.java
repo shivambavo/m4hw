45979250a06b370efb4720fe74f070a79d2bdc7a
Shivam Bavaria <shivam.bavaria@gmail.com>
2025-11-24 02:10:16 -0500

    checksytle stuff

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,39 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance of a play.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private final String playID;
+    private final int audience;
 
+    /**
+     * Constructs a new Performance.
+     *
+     * @param playID   the ID of the play being performed
+     * @param audience the number of people in the audience
+     */
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
+    }
+
+    /**
+     * Gets the play ID for this performance.
+     *
+     * @return the play ID
+     */
+    public String getPlayID() {
+        return playID;
+    }
+
+    /**
+     * Gets the audience count for this performance.
+     *
+     * @return the audience count
+     */
+    public int getAudience() {
+        return audience;
     }
 }
--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,39 @@
 package theater;
 
+/**
+ * Represents a play, containing its name and type (e.g., tragedy, comedy).
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
+    /**
+     * Constructs a new Play.
+     *
+     * @param name the name of the play
+     * @param type the type of the play (e.g., "tragedy", "comedy")
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    /**
+     * Gets the name of the play.
+     *
+     * @return the play's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets the type of the play.
+     *
+     * @return the play's type
+     */
+    public String getType() {
+        return type;
     }
 }
